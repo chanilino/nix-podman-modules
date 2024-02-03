0a8c70989a65eebf3bf7b9def6cf7dece4c6b102
Chanilino <chan@example.com>
2024-02-03 16:53:45 +0100

    Added another module

--- flake.nix
+++ flake.nix
@@ -11,7 +11,8 @@
        #in 
        {
        nixosModules.home-podman-rootless = import ./home-modules/podman-rootless-containers.nix;
-       nixosModules.default = self.home-podman-rootless;
+       nixosModules.home-podman-compose-rootless = import ./home-modules/podman-compose-rootless.nix;
+       #nixosModules.default = self.home-podman-rootless;
 
    };
  }
